8c2ecd0a823a3818b1ffe3bc882751ebcdd1076c
zhang ziyue <zhangziyue@example.com>
2025-12-23 09:23:43 +0800

    anti-duplicate random seed

--- src/rendering/layers/JSONComposition.cpp
+++ src/rendering/layers/JSONComposition.cpp
@@ -746,7 +746,7 @@ PreComposeLayer* createVideoLayer(movie::VideoTrack* track, const movie::MovieSp
     //zzy, add effect for “查重”
     if (track->content.effect == "anti-duplicate") {
       CornerPinEffect* effect = new CornerPinEffect();
-      int random_height_offset = pag::get_random_int(20, 50);
+      int random_height_offset = pag::get_random_int(15, 30);
       effect->upperLeft = new Property<Point>(pag::Point::Make(0, 0 - random_height_offset)); 
       effect->upperRight = new Property<Point>(pag::Point::Make(video_width, 0));
       effect->lowerLeft = new Property<Point>(pag::Point::Make(0, video_height + random_height_offset));
